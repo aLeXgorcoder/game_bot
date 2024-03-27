21b031b8c171a89e740da3ae3bdb3d3bcf66dc92
aLeXgorcoder <aleksejgoryansky@yandex.ru>
2024-03-27 20:11:32 +0300

    init

---
+++ keyboards/main_menu.py
@@ -0,0 +1,4 @@
+from aiogram import Bot
+
+#async def set_main_menu(bot: Bot):
+    #main_menu_command =
---
+++ main.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher
+from config.config import TOKEN
+
+
+#async def main():
+    #bot = Bot(TOKEN)
+    #dp = Dispatcher()
+    #await set_main_menu
+
+
+
+
+
+
